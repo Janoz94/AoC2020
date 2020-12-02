d06d669182ec5b613f3021bbfa23248b8443aa0e
Jan Rutkowski <janek.rut94@gmail.com>
2020-12-02 18:20:43 +0100

    Adding solution of day 2 part 1.

--- AdventOfCode2020.playground/Contents.swift
+++ AdventOfCode2020.playground/Contents.swift
@@ -7,3 +7,7 @@ let aoc1 = Aoc1(arrayOfNumbers: Aoc1Helper.readFile("/Users/jrutkowski/Documents
 print("Advent of code day 1 part 1 solution: \(aoc1.findTwoNumbersWhichSumTo2020AndMultiplyThem())")
 
 print("Advent of code day 1 part 1 solution: \(aoc1.findThreeNumbersWhichSumTo2020AndMultiplyThem())")
+
+
+let aoc2 = Aoc2(arrayOfValues: Aoc2Helper.readFile("/Users/jrutkowski/Documents/Projects/AoC2020/AdventOfCode2020.playground/Resources/aoc2Input"))
+print("Advent of code day 2 part 1 solution: \(aoc2.checkAmountOfProperPasswords())")
